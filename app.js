a9c2b70594c4b0fed8de7a8c9161c1e7e4f30eaf
mjh1985codeman <Michael.Hodges1985@Yahoo.com>
2021-08-04 20:53:10 -0500

    added src folder and page-template.js file and generated the HTML page using JS

--- app.js
+++ app.js
@@ -1,23 +1,12 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+
+const profileDataArgs = process.argv.slice(2);
+
 const [name, github] = profileDataArgs;
 
-const generatePage = (userName, githubName) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
+fs.writeFile("./index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
-console.log(name, github);
-console.log(generatePage(name, github)); //This pushed "Jane" and "janehub" into the generatePage arrow function as the userName and githubName arguments.
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
